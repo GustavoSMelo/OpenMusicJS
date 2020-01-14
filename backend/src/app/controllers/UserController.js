451b96d6a30336e5e701689252d538c167395a4f
Gustavo Santos Melo <gsantos15569@gmail.com>
2020-01-14 00:49:29 -0300

    add a destroy method

--- backend/src/app/controllers/UserController.js
+++ backend/src/app/controllers/UserController.js
@@ -1,6 +1,8 @@
 const user = require('../models/user');
 const jwt = require('jsonwebtoken');
 const bcrypt = require('bcryptjs');
+const { promisify } = require('util');
+const tokenConfig = require('../../config/token/tokenConfig');
 
 module.exports = {
     async store(req, res) {
@@ -28,12 +30,37 @@ module.exports = {
     async index(req, res) {
         const users = await user.findAll();
 
-        if (!users) {
+        if (users.length <= 0) {
             return res
                 .status(404)
                 .json({ Error: 'Any account is registred in database ' });
         }
 
         return res.json(users);
+    },
+
+    async destroy(req, res) {
+        const authHeader = req.headers.authorization;
+
+        const [_, token] = authHeader.split(' ');
+
+        const infoToken = await promisify(jwt.verify)(
+            token,
+            tokenConfig.secret
+        );
+
+        const deleted = await user.destroy({
+            where: {
+                id: infoToken.id,
+            },
+        });
+
+        if (!deleted) {
+            return res
+                .status(401)
+                .json({ Error: 'Token expires!, please, do login again ' });
+        }
+
+        return res.json({ message: 'User deleted with success! ' });
     },
 };
