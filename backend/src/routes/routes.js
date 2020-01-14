356fe845c31b2d19d5f3327b11d877a96aafe76d
Gustavo Santos Melo <gsantos15569@gmail.com>
2020-01-14 00:50:00 -0300

    add routes of to delete and to login

--- backend/src/routes/routes.js
+++ backend/src/routes/routes.js
@@ -1,8 +1,12 @@
 const express = require('express');
 const routes = express.Router();
 const UserController = require('../app/controllers/UserController');
+const SessionController = require('../app/controllers/SessionController');
+const authMiddleware = require('../app/middlewares/auth/authMiddleware');
 
 routes.post('/user', UserController.store);
 routes.get('/user', UserController.index);
+routes.delete('/user', authMiddleware, UserController.destroy);
+routes.post('/login', SessionController.store);
 
 module.exports = routes;
