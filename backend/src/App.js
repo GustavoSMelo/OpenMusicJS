382e9f5a6d862c7da4041f4f68b2f1cb5d84c811
Gustavo Santos Melo <gsantos15569@gmail.com>
2020-01-21 22:47:31 -0300

    use cors

--- backend/src/App.js
+++ backend/src/App.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const app = express();
 const routes = require('./routes/routes');
+const cors = require('cors');
 require('./database/index');
 
+app.use(cors());
 app.use(express.json());
 app.use('/', routes);
 
