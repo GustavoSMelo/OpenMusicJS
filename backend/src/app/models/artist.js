d217f4a88a36be71dd7b05ebcd12664e2ed1467b
Gustavo Santos Melo <gsantos15569@gmail.com>
2020-01-21 22:49:53 -0300

    model of artist

---
+++ backend/src/app/models/artist.js
@@ -0,0 +1,17 @@
+const { DataTypes, Model } = require('sequelize');
+
+class Artist extends Model {
+    static init(connection) {
+        super.init(
+            {
+                name: DataTypes.STRING,
+                name_artistic: DataTypes.STRING,
+                email: DataTypes.STRING,
+                password: DataTypes.STRING,
+            },
+            { sequelize: connection }
+        );
+    }
+}
+
+module.exports = Artist;
