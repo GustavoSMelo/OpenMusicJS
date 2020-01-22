f4cc597d5ceba9bb0f2ef9d11edffeee89fdfd55
Gustavo Santos Melo <gsantos15569@gmail.com>
2020-01-21 22:50:11 -0300

    migration of artist

---
+++ backend/src/database/migrations/20200121203426-create_artist.js
@@ -0,0 +1,49 @@
+'use strict';
+
+module.exports = {
+    up: (queryInterface, Sequelize) => {
+        return queryInterface.createTable('artists', {
+            id: {
+                type: Sequelize.INTEGER,
+                primaryKey: true,
+                allowNull: false,
+                autoIncrement: true,
+            },
+            name: {
+                type: Sequelize.STRING,
+                allowNull: false,
+            },
+
+            name_artistic: {
+                type: Sequelize.STRING,
+                allowNull: false,
+                unique: true,
+            },
+
+            email: {
+                type: Sequelize.STRING,
+                allowNull: false,
+                unique: true,
+            },
+
+            password: {
+                type: Sequelize.STRING,
+                allowNull: false,
+            },
+
+            created_at: {
+                type: Sequelize.DATE,
+                allowNull: false,
+            },
+
+            updated_at: {
+                type: Sequelize.DATE,
+                allowNull: false,
+            },
+        });
+    },
+
+    down: queryInterface => {
+        return queryInterface.dropTable('artists');
+    },
+};
