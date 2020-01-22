20decca97add40ee5c4099412fcba2e5f91fad20
Gustavo Santos Melo <gsantos15569@gmail.com>
2020-01-21 22:48:12 -0300

    add model of artista

--- backend/src/database/index.js
+++ backend/src/database/index.js
@@ -1,8 +1,10 @@
 const Sequelize = require('sequelize');
 const dbConfig = require('../config/sql/config');
 const UserModel = require('../app/models/user');
+const ArtistModel = require('../app/models/artist');
 
 const connection = new Sequelize(dbConfig);
 UserModel.init(connection);
+ArtistModel.init(connection);
 
 module.exports = connection;
