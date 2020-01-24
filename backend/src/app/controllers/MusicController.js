dd916f4f7bb4ec8979f8502a0dc5295c99d9f4df
Gustavo Santos Melo <gsantos15569@gmail.com>
2020-01-24 18:26:49 -0300

    Method destroy and updated ready

--- backend/src/app/controllers/MusicController.js
+++ backend/src/app/controllers/MusicController.js
@@ -1,4 +1,5 @@
 const musics = require('../models/music');
+const authMethod = require('../../utils/authMethod');
 
 module.exports = {
     async store(req, res) {
@@ -42,13 +43,123 @@ module.exports = {
         const allmusics = await musics.findAll();
 
         if (!allmusics || allmusics.length <= 0) {
-            return res
-                .status(404)
-                .json({
-                    Error: 'Any music did registre or finded in database ',
-                });
+            return res.status(404).json({
+                Error: 'Any music did registre or finded in database ',
+            });
         }
 
         return res.json(allmusics);
+    },
+
+    async destroy(req, res) {
+        const authHeader = req.headers.authorization;
+
+        const info = await authMethod(authHeader);
+
+        if (!info || info.length <= 0) {
+            return res.status(401).json({ Error: 'Token inst confirmed ' });
+        }
+
+        const { id: idArtist } = info;
+
+        const { name, id } = req.body;
+
+        if (!name || !id) {
+            return res.status(400).json({
+                Error: 'Please, complete all the fields to continue ',
+            });
+        }
+
+        const musicfinded = await musics.findOne({
+            where: {
+                id,
+            },
+        });
+
+        if (!musicfinded) {
+            return res
+                .status(404)
+                .json({ Error: 'Music not found in we database ' });
+        }
+
+        if (name !== musicfinded.name) {
+            return res
+                .status(401)
+                .json({ Error: 'Id and name of music dont match ' });
+        }
+
+        if (idArtist !== musicfinded.singer) {
+            return res
+                .status(401)
+                .json({ Error: 'Your account inst owner this music ' });
+        }
+
+        await musics.destroy({ where: { id } });
+
+        return res.json({ message: 'Music deleted with success! ' });
+    },
+
+    async update(req, res) {
+        const authHeader = req.headers.authorization;
+
+        const info = await authMethod(authHeader);
+
+        if (!info || info.length <= 0) {
+            return res.status(401).json({ Error: 'Token inst confirmed ' });
+        }
+
+        const { id: idArtist } = info;
+
+        const { id } = req.body;
+
+        if (!id) {
+            return res.status(400).json({ Error: 'Id not sended ' });
+        }
+
+        const musicfinded = await musics.findOne({ where: { id } });
+
+        if (!musicfinded) {
+            return res
+                .status(404)
+                .json({ Error: 'Any music finded with this id ' });
+        }
+
+        if (idArtist !== musicfinded.singer) {
+            return res
+                .status(401)
+                .json({ Error: 'Your account inst owner this music ' });
+        }
+
+        const {
+            oldname = musicfinded.name,
+            newname: name,
+            genre = musicfinded.genre,
+            singer = musicfinded.singer,
+        } = req.body;
+
+        if (!oldname || !name || !genre || !singer) {
+            return res.status(400).json({
+                Error: 'Please, complete all the fields to continue ',
+            });
+        }
+
+        if (oldname !== musicfinded.name) {
+            return res
+                .status(404)
+                .json({ Error: 'oldname of music isnt valid ' });
+        }
+
+        try {
+            await musics.update(
+                { oldname, name, genre, singer },
+                { where: { id } }
+            );
+
+            return res.json({ message: 'Music updated with success ' });
+        } catch (err) {
+            return res.status(500).json({
+                Error: `Ops, we have some error in server, please try to update again \n${err}`,
+            });
+        }
     },
 };
