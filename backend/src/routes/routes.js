fa5d81e1c06490daf23776f469d15be38e73c798
Gustavo Santos Melo <gsantos15569@gmail.com>
2020-01-23 23:34:34 -0300

    add configuration to upload archives

--- backend/src/routes/routes.js
+++ backend/src/routes/routes.js
@@ -5,19 +5,36 @@ const SessionController = require('../app/controllers/SessionController');
 const authMiddleware = require('../app/middlewares/auth/authMiddleware');
 const ArtistController = require('../app/controllers/ArtistController');
 const ArtSessionController = require('../app/controllers/ArtSessionController');
+const multer = require('multer');
+const multerConfig = require('../config/uploads/multer');
+
+const upload = multer(multerConfig);
 
 //routes of user
-routes.post('/user', UserController.store);
+routes.post('/user', upload.single('avatar'), UserController.store);
 routes.get('/user', UserController.index);
 routes.delete('/user', authMiddleware, UserController.destroy);
 routes.post('/login/user', SessionController.store);
-routes.put('/user', authMiddleware, UserController.update);
+routes.put(
+    '/user',
+    authMiddleware,
+    upload.single('avatar'),
+    UserController.update
+);
+routes.post('/files', upload.single('avatar'), (req, res) => {
+    return res.json(req.file);
+});
 
 //routes of artist
-routes.post('/artist', ArtistController.store);
+routes.post('/artist', upload.single('avatar'), ArtistController.store);
 routes.get('/artist', ArtistController.index);
 routes.delete('/artist', authMiddleware, ArtistController.destroy);
-routes.put('/artist', authMiddleware, ArtistController.update);
+routes.put(
+    '/artist',
+    authMiddleware,
+    upload.single('avatar'),
+    ArtistController.update
+);
 routes.post('/login/artist', ArtSessionController.store);
 
 module.exports = routes;
