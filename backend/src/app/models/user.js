da4880d408a3b0b0f26803a7e54504f0ad835f40
Gustavo Santos Melo <gsantos15569@gmail.com>
2020-01-23 23:33:55 -0300

    add avatar field in model of users

--- backend/src/app/models/user.js
+++ backend/src/app/models/user.js
@@ -1,18 +1,19 @@
 const { Model, DataTypes } = require('sequelize');
 
 class User extends Model {
-  static init(connection) {
-    super.init(
-      {
-        name: DataTypes.STRING,
-        email: DataTypes.STRING,
-        password: DataTypes.STRING
-      },
-      {
-        sequelize: connection
-      }
-    );
-  }
+    static init(connection) {
+        super.init(
+            {
+                name: DataTypes.STRING,
+                email: DataTypes.STRING,
+                password: DataTypes.STRING,
+                avatar: DataTypes.STRING,
+            },
+            {
+                sequelize: connection,
+            }
+        );
+    }
 }
 
 module.exports = User;
