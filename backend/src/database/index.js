287f44cf505d83e55c0ba382b0b552a47df9b2fb
Gustavo Santos Melo <gsantos15569@gmail.com>
2020-01-24 02:02:31 -0300

    add model of music and relationship of music and artist

--- backend/src/database/index.js
+++ backend/src/database/index.js
@@ -2,9 +2,13 @@ const Sequelize = require('sequelize');
 const dbConfig = require('../config/sql/config');
 const UserModel = require('../app/models/user');
 const ArtistModel = require('../app/models/artist');
+const MusicModel = require('../app/models/music');
 
 const connection = new Sequelize(dbConfig);
 UserModel.init(connection);
 ArtistModel.init(connection);
+MusicModel.init(connection);
+
+MusicModel.belongsTo(ArtistModel, { foreignKey: 'singer' });
 
 module.exports = connection;
