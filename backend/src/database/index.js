17bacbe8fd6fad57e5b8e68fc9875390e1d628ac
Gustavo Santos Melo <gsantos15569@gmail.com>
2020-01-25 23:41:03 -0300

    add model of albuns

--- backend/src/database/index.js
+++ backend/src/database/index.js
@@ -3,11 +3,13 @@ const dbConfig = require('../config/sql/config');
 const UserModel = require('../app/models/user');
 const ArtistModel = require('../app/models/artist');
 const MusicModel = require('../app/models/music');
+const AlbunsModel = require('../app/models/albuns');
 
 const connection = new Sequelize(dbConfig);
 UserModel.init(connection);
 ArtistModel.init(connection);
 MusicModel.init(connection);
+AlbunsModel.init(connection);
 
 MusicModel.belongsTo(ArtistModel, { foreignKey: 'singer' });
 
