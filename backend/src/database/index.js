a71c00ecded5814d969e92c5f95cf2cbef31deac
Gustavo Santos Melo <gsantos15569@gmail.com>
2020-01-27 02:01:15 -0300

    add new relationship

--- backend/src/database/index.js
+++ backend/src/database/index.js
@@ -5,12 +5,15 @@ const ArtistModel = require('../app/models/artist');
 const MusicModel = require('../app/models/music');
 const AlbunsModel = require('../app/models/albuns');
 
+//connections
 const connection = new Sequelize(dbConfig);
 UserModel.init(connection);
 ArtistModel.init(connection);
 MusicModel.init(connection);
 AlbunsModel.init(connection);
 
+//relationship
 MusicModel.belongsTo(ArtistModel, { foreignKey: 'singer' });
+AlbunsModel.belongsTo(ArtistModel, { foreignKey: 'artist' });
 
 module.exports = connection;
