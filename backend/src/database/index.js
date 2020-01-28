9ab4857206922cbdc3069266ae2f2cef23617cd4
Gustavo Santos Melo <gsantos15569@gmail.com>
2020-01-28 17:04:57 -0300

    new connections and relationships

--- backend/src/database/index.js
+++ backend/src/database/index.js
@@ -4,6 +4,7 @@ const UserModel = require('../app/models/user');
 const ArtistModel = require('../app/models/artist');
 const MusicModel = require('../app/models/music');
 const AlbunsModel = require('../app/models/albuns');
+const RelationshipAAM = require('../app/models/relationship_album_music_artist');
 
 //connections
 const connection = new Sequelize(dbConfig);
@@ -11,9 +12,13 @@ UserModel.init(connection);
 ArtistModel.init(connection);
 MusicModel.init(connection);
 AlbunsModel.init(connection);
+RelationshipAAM.init(connection);
 
 //relationship
 MusicModel.belongsTo(ArtistModel, { foreignKey: 'singer' });
 AlbunsModel.belongsTo(ArtistModel, { foreignKey: 'artist' });
+RelationshipAAM.belongsTo(ArtistModel, { foreignKey: 'artists' });
+RelationshipAAM.belongsTo(MusicModel, { foreignKey: 'music' });
+RelationshipAAM.belongsTo(AlbunsModel, { foreignKey: 'album' });
 
 module.exports = connection;
