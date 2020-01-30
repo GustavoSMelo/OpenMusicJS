ecbeb1b10294e346c1b8eebc05b81ad7ac635cf2
Gustavo Santos Melo <gsantos15569@gmail.com>
2020-01-30 19:04:40 -0300

    method to search musics, artists, albuns and users

---
+++ backend/src/utils/SearchMethod.js
@@ -0,0 +1,74 @@
+const Sequelize = require('sequelize');
+const music = require('../app/models/music');
+const artist = require('../app/models/artist');
+const user = require('../app/models/user');
+const album = require('../app/models/albuns');
+
+module.exports = async (req, res) => {
+    const { searchString } = req.body;
+
+    if (!searchString) {
+        return res.status(400).json({
+            Error: 'Please, complete all the fields to continue ur action ',
+        });
+    }
+
+    const op = Sequelize.Op;
+    const info = [];
+
+    const SearchMusic = await music.findAll({
+        where: {
+            name: {
+                [op.like]: `%${searchString}%`,
+            },
+        },
+    });
+
+    if (SearchMusic.length >= 1) {
+        info.push(SearchMusic);
+    }
+
+    const SearchArtists = await artist.findAll({
+        where: {
+            name_artistic: {
+                [op.like]: `%${searchString}%`,
+            },
+        },
+    });
+
+    if (SearchArtists.length >= 1) {
+        info.push(SearchArtists);
+    }
+
+    const SearchAlbum = await album.findAll({
+        where: {
+            name: {
+                [op.like]: `%${searchString}%`,
+            },
+        },
+    });
+
+    if (SearchAlbum.length >= 1) {
+        info.push(SearchAlbum);
+    }
+
+    const SearchUsers = await user.findAll({
+        where: {
+            name: {
+                [op.like]: `%${searchString}%`,
+            },
+        },
+    });
+
+    if (SearchUsers.length >= 1) {
+        info.push(SearchUsers);
+    }
+
+    if (!info || info.length <= 0) {
+        return res
+            .status(404)
+            .json({ Error: 'Any artist, music, album or user is finded ' });
+    }
+
+    return res.json(info);
+};
