a3508d819d809a27d71ac35a6654994a05fb0cfe
Gustavo Santos Melo <gsantos15569@gmail.com>
2020-01-30 01:19:11 -0300

    users-like-artists relationship controller

---
+++ backend/src/app/controllers/users_like_artistsController.js
@@ -0,0 +1,103 @@
+const Artists = require('../models/artist');
+const authMethod = require('../../utils/authMethod');
+const users_like_artists = require('../models/users_like_artists');
+
+module.exports = {
+    async store(req, res) {
+        const authHeader = req.headers.authorization;
+
+        const { id: user } = await authMethod(authHeader);
+
+        const { artist } = req.body;
+
+        if (!artist) {
+            return res
+                .status(400)
+                .json({ Error: 'Please, insert all fields to continue ' });
+        }
+
+        const isArtistExists = await Artists.findOne({
+            where: {
+                id: artist,
+            },
+        });
+
+        if (!isArtistExists) {
+            return res.status(400).json({ Error: 'Artists doesnt exists ' });
+        }
+
+        const isLikeExists = await users_like_artists.findOne({
+            where: {
+                artist,
+                user,
+            },
+        });
+
+        if (isLikeExists) {
+            return res
+                .status(400)
+                .json({ Error: 'U already like this album ' });
+        }
+
+        await users_like_artists.create({ user, artist });
+
+        return res.json({ message: 'Artist liked with success ' });
+    },
+
+    async index(req, res) {
+        const authHeader = req.headers.authorization;
+
+        const { id: user } = await authMethod(authHeader);
+
+        const likes = await users_like_artists.findAll({
+            where: {
+                user,
+            },
+        });
+
+        if (!likes || likes.length <= 0) {
+            return res
+                .status(404)
+                .json({ Error: 'This user does not like any artist ' });
+        }
+
+        return res.json(likes);
+    },
+
+    async destroy(req, res) {
+        const authHeader = req.headers.authorization;
+
+        const { id: user } = await authMethod(authHeader);
+
+        const { artist } = req.body;
+
+        const isArtistExists = await Artists.findOne({ where: { id: artist } });
+
+        if (!isArtistExistsExists) {
+            return res.status(404).json({ Error: 'Artist didnt find ' });
+        }
+
+        const isLiked = await users_like_artists.findOne({
+            where: {
+                user,
+                artist,
+            },
+        });
+
+        if (!isLiked) {
+            return res
+                .status(400)
+                .json({ Error: 'U dont like this artist to remove ur like ' });
+        }
+
+        await users_like_artists.destroy({
+            where: {
+                user,
+                artist,
+            },
+            limit: 1,
+        });
+
+        return res.json({ message: 'Like Removed with success! ' });
+    },
+};
