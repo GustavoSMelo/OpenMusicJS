270fc5005871359cfb6655724d87268349304d56
Gustavo Santos Melo <gsantos15569@gmail.com>
2020-02-04 23:52:16 -0300

    complete view of main page

--- frontend/user/src/views/main/index.js
+++ frontend/user/src/views/main/index.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import Logo from '../../assets/img/logo.png';
-import { Container } from './styled';
+import { Container, Image } from './styled';
+import backbubbles from '../../assets/img/bubbles-main.jpg';
+import { FaMusic } from 'react-icons/fa';
+import Footer from '../../components/footer';
 
 function Main() {
     return (
         <Container>
             <header>
-                <img src={Logo} />
+                <img src={Logo} alt="logo-icon" />
                 <h1>
                     Musicfy, <br />
                     Listen all your musics here!
@@ -16,6 +19,37 @@ function Main() {
                 <button>LogIn to Listen</button>
                 <a href="/">create account for free </a>
             </main>
+            <Image img={backbubbles}>
+                <h1>Dive in the world of musics with Musicfy</h1>
+                <br />
+                <h2>For Free</h2>
+
+                <section className="for-artists">
+                    <p>
+                        <FaMusic /> If you want post your music here,
+                        <br /> its only to create a account
+                        <br /> how to artists and post in same time <FaMusic />
+                        <br />
+                    </p>
+
+                    <br />
+                    <button type="button">LogIn here</button>
+                    <br />
+                    <a href="/">Create account</a>
+                </section>
+
+                <br />
+                <section className="for-mobile">
+                    <p>
+                        And, if you want to listen your music in anywhere,{' '}
+                        <br />
+                        please, install we app. it's developed think about you
+                    </p>
+                </section>
+                <br />
+            </Image>
+
+            <Footer />
         </Container>
     );
 }
