1d59edcdc4fa79c90dd561a2e969f964c2046f56
Gustavo Santos Melo <gsantos15569@gmail.com>
2020-02-05 23:44:53 -0300

    new routes in the project

--- frontend/user/src/routes/routes.js
+++ frontend/user/src/routes/routes.js
@@ -3,6 +3,10 @@ import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import Main from '../views/main';
 import ChooseLogin from '../views/chooseLogin';
 import ChooseSign from '../views/chooseSign';
+import LoginArtist from '../views/LoginArtist';
+import LoginUser from '../views/LoginUser';
+import SignUser from '../views/SignUser';
+import SignArtist from '../views/SignArtist';
 
 function Routes() {
     return (
@@ -11,6 +15,10 @@ function Routes() {
                 <Route exact path="/" component={Main} />
                 <Route exact path="/login" component={ChooseLogin} />
                 <Route exact path="/sign" component={ChooseSign} />
+                <Route exact path="/login/artist" component={LoginArtist} />
+                <Route exact path="/login/user" component={LoginUser} />
+                <Route exact path="/sign/user" component={SignUser} />
+                <Route exact path="/sign/artist" component={SignArtist} />
             </Switch>
         </BrowserRouter>
     );
