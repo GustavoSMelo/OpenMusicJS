067498b00cd0601eec7ff07793cc2f7b1aebd388
Gustavo Santos Melo <gsantos15569@gmail.com>
2020-02-07 21:50:55 -0300

    change button to link

--- frontend/user/src/views/chooseSign/index.js
+++ frontend/user/src/views/chooseSign/index.js
@@ -1,12 +1,17 @@
 import React from 'react';
 import { Container } from './styled';
 import Footer from '../../components/footer';
+import { Link } from 'react-router-dom';
 
 function ChooseSign() {
     return (
         <Container>
-            <button>SignIn with user </button>
-            <button>SignIn with artist </button>
+            <Link className="button" to="/sign/user">
+                SignIn with user{' '}
+            </Link>
+            <Link className="button" to="/sign/artist">
+                SignIn with artist{' '}
+            </Link>
             <Footer />
         </Container>
     );
