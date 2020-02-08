a591bfff2cf3a6edc7dc9f1996d40f1713120f1b
Gustavo Santos Melo <gsantos15569@gmail.com>
2020-02-07 21:40:55 -0300

    font changed

--- frontend/user/src/style/global.js
+++ frontend/user/src/style/global.js
@@ -5,7 +5,7 @@ export default createGlobalStyle`
         padding: 0;
         margin: 0;
         box-sizing: border-box;
-        font-family: Verdana, Geneva, Tahoma, sans-serif
+        font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif
     }
 
     html, border-style, #root{
