a2b7c2f9d504a261923299f0d04e4a6bf9d99c90
Gustavo Santos Melo <gsantos15569@gmail.com>
2020-02-11 23:28:12 -0300

    add limit size to archives sended by frontend

--- backend/src/App.js
+++ backend/src/App.js
@@ -5,7 +5,8 @@ const cors = require('cors');
 require('./database/index');
 
 app.use(cors());
-app.use(express.json());
+app.use(express.json({ limit: '512mb' }));
+app.use(express.urlencoded({ extended: true, limit: '512mb' }));
 app.use('/', routes);
 
 const PORT = 3333;
