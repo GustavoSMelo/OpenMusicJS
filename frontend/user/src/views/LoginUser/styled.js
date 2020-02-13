d9542e1310bff34b66d0bc3a5f4d327aec5b8f2f
Gustavo Santos Melo <gsantos15569@gmail.com>
2020-02-12 23:41:14 -0300

    add status error of login to user

--- frontend/user/src/views/LoginUser/styled.js
+++ frontend/user/src/views/LoginUser/styled.js
@@ -1,6 +1,15 @@
-import styled from 'styled-components';
+import styled, { keyframes } from 'styled-components';
 
-export const Container = styled.div`
+const animationStatus = keyframes`
+    from{
+        transform: translateX(-30deg) scale(-0.8);
+    }
+    to{
+        transform: translateX(0deg) scale(0);
+    }
+`;
+
+export const Container = styled.main`
     background-image: url(${props => props.img});
     height: 100vh;
     background-size: cover;
@@ -42,5 +51,23 @@ export const Container = styled.div`
     h1 {
         margin: 20px;
         color: #fff;
+    }
+`;
+
+export const ContainerError = styled.section`
+    display: flex;
+    width: 45%;
+    background-color: #e88a89 !important;
+    color: #ff0500 !important;
+    padding: 30px;
+    font-weight: bold;
+    text-align: center;
+    border-radius: 5px;
+    animation: ${animationStatus} 2s;
+    flex-direction: column;
+    margin: 30px;
+
+    @media screen and (max-width: 780px) {
+        margin: 0px;
     }
 `;
