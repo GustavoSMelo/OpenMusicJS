5b58f585b1f4ac8e0b2375d447e2f8e88f703464
Gustavo Santos Melo <gsantos15569@gmail.com>
2020-02-14 18:00:37 -0300

    Add view of token not found (user not logged)

--- frontend/user/src/views/Home/index.js
+++ frontend/user/src/views/Home/index.js
@@ -1,10 +1,11 @@
 import React, { useEffect, useState } from 'react';
 import authToken from '../../utils/authToken';
 import Navbar from '../../components/navbar';
-import { Container } from './styled';
+import { Container, ContainerNotLogged } from './styled';
 import { FaThumbsUp, FaHeadphones } from 'react-icons/fa';
 import imgTest from '../../assets/img/login-theme2.jpg';
 import Footer from '../../components/footer';
+import { Link } from 'react-router-dom';
 
 function Home() {
     const [info, setInfo] = useState('');
@@ -12,8 +13,11 @@ function Home() {
     useEffect(() => {
         async function getDataByAPI() {
             const data = await authToken('/musics');
-            setInfo(data.data);
-            console.log(data.data);
+
+            if (!data) {
+                return setInfo(null);
+            }
+            return setInfo(data.data);
         }
 
         getDataByAPI();
@@ -26,22 +30,31 @@ function Home() {
                 <Container>
                     <h1>New musics: </h1>
                     <ul>
-                        {info.map(item => (
-                            <li key={item.id}>
-                                <img src={imgTest} alt="banner of music" />
-                                <p>{item.name}</p>
-                                <p>{item.genre}</p>
+                        {info ? (
+                            info.map(item => (
+                                <li key={item.id}>
+                                    <img src={imgTest} alt="banner of music" />
+                                    <p>{item.name}</p>
+                                    <p>{item.genre}</p>
 
-                                <span>
-                                    <button className="listen" type="button">
-                                        <FaHeadphones /> Listen
-                                    </button>
-                                    <button className="like" type="button">
-                                        <FaThumbsUp />
-                                    </button>
-                                </span>
-                            </li>
-                        ))}
+                                    <span>
+                                        <button
+                                            className="listen"
+                                            type="button"
+                                        >
+                                            <FaHeadphones /> Listen
+                                        </button>
+                                        <button className="like" type="button">
+                                            <FaThumbsUp />
+                                        </button>
+                                    </span>
+                                </li>
+                            ))
+                        ) : (
+                            <h1>
+                                Does not have any music registred in database{' '}
+                            </h1>
+                        )}
                     </ul>
                 </Container>
                 <Footer />
@@ -50,7 +63,14 @@ function Home() {
     }
 
     function LayoutNotLogged() {
-        return <h1>No</h1>;
+        return (
+            <ContainerNotLogged>
+                <h1>To continue, please make login</h1>
+                <Link className="btnLogin" to="/login/user">
+                    Login
+                </Link>
+            </ContainerNotLogged>
+        );
     }
 
     return <>{info ? LayoutLogged() : LayoutNotLogged()}</>;
