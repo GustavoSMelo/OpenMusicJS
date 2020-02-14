29a29ce52731f2c33d0ec513289ba1193b86a710
Gustavo Santos Melo <gsantos15569@gmail.com>
2020-02-13 21:59:34 -0300

    implementing of logic to home page

--- frontend/user/src/views/Home/index.js
+++ frontend/user/src/views/Home/index.js
@@ -1,19 +1,59 @@
 import React, { useEffect, useState } from 'react';
 import authToken from '../../utils/authToken';
+import Navbar from '../../components/navbar';
+import { Container } from './styled';
+import { FaThumbsUp, FaHeadphones } from 'react-icons/fa';
+import imgTest from '../../assets/img/login-theme2.jpg';
+import Footer from '../../components/footer';
 
 function Home() {
     const [info, setInfo] = useState('');
-    const DidMount = useEffect(() => {
+
+    useEffect(() => {
         async function getDataByAPI() {
             const data = await authToken('/musics');
-            setInfo(data);
-            console.log(data);
+            setInfo(data.data);
+            console.log(data.data);
         }
 
         getDataByAPI();
     }, []);
 
-    return <>{info ? <h1>Yes</h1> : <h1>No</h1>}</>;
+    function LayoutLogged() {
+        return (
+            <>
+                <Navbar />
+                <Container>
+                    <h1>New musics: </h1>
+                    <ul>
+                        {info.map(item => (
+                            <li key={item.id}>
+                                <img src={imgTest} alt="banner of music" />
+                                <p>{item.name}</p>
+                                <p>{item.genre}</p>
+
+                                <span>
+                                    <button className="listen" type="button">
+                                        <FaHeadphones /> Listen
+                                    </button>
+                                    <button className="like" type="button">
+                                        <FaThumbsUp />
+                                    </button>
+                                </span>
+                            </li>
+                        ))}
+                    </ul>
+                </Container>
+                <Footer />
+            </>
+        );
+    }
+
+    function LayoutNotLogged() {
+        return <h1>No</h1>;
+    }
+
+    return <>{info ? LayoutLogged() : LayoutNotLogged()}</>;
 }
 
 export default Home;
