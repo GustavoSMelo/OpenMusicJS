3b98c51794b35b83ddb5624c27c60c2158183afa
Gustavo Santos Melo <gsantos15569@gmail.com>
2020-02-25 00:34:08 -0300

    style of mp3 player

---
+++ frontend/src/components/player/styled.js
@@ -0,0 +1,12 @@
+import styled from 'styled-components';
+
+export const MP3Player = styled.audio`
+    display: flex;
+    background-color: #202020;
+    color: #c200ff;
+    flex-direction: row;
+    align-items: center;
+    justify-content: space-between;
+    width: 100%;
+    padding: 20px;
+`;
