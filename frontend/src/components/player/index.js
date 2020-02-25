1f0e4fcdb102c9d5a5adc8c7e1835d95d6dbea56
Gustavo Santos Melo <gsantos15569@gmail.com>
2020-02-24 23:42:26 -0300

    player to listen musics save in database in access by backend

---
+++ frontend/src/components/player/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Player() {
+    <audio controls>
+        <source
+            src={`http://localhost:3333/music/${props.musicname}`}
+            type="audio/mpeg"
+        />
+    </audio>;
+}
+
+export default Player;
