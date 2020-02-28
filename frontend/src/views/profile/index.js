8ea0870fac8b646dc1268f64948913dcca2c3140
Gustavo Santos Melo <gsantos15569@gmail.com>
2020-02-27 22:40:04 -0300

    listing the albuns, musics and artists that user liked

--- frontend/src/views/profile/index.js
+++ frontend/src/views/profile/index.js
@@ -2,17 +2,37 @@ import React, { useEffect, useState } from 'react';
 import authToken from '../../utils/authToken';
 import Navbar from '../../components/navbar';
 import Footer from '../../components/footer';
+import {
+    FaEnvelope,
+    FaPen,
+    FaTrash,
+    FaFolderOpen,
+    FaMusic,
+    FaBroom,
+} from 'react-icons/fa';
 import { Container } from './styled';
-import { FaEnvelope, FaPen, FaTrash } from 'react-icons/fa';
 
 function Profile() {
     const [user, setUser] = useState('');
+    const [UserAlbunsFavorites, setUserAlbunsFavorites] = useState('');
+    const [UserMusicsFavorites, setUserMusicsFavorites] = useState('');
+    const [UserArtistsFavorites, setUserArtistsFavorites] = useState('');
 
     useEffect(() => {
         async function getApiData() {
             try {
                 const datas = await authToken('/user/show');
+                console.log(datas);
                 await setUser(datas.data.user);
+                await setUserAlbunsFavorites(
+                    datas.data.user_albuns.dataUserAlbuns
+                );
+                await setUserArtistsFavorites(
+                    datas.data.user_artist.dataUserArtists
+                );
+                await setUserMusicsFavorites(
+                    datas.data.user_musics.dataUserMusics
+                );
             } catch (err) {
                 console.error('Some error happens ');
             }
@@ -22,6 +42,9 @@ function Profile() {
 
     function RenderWithUser() {
         console.log(user);
+        console.log(UserAlbunsFavorites);
+        console.log(UserMusicsFavorites);
+        console.log(UserArtistsFavorites);
         return (
             <>
                 <Navbar />
@@ -41,6 +64,46 @@ function Profile() {
                             <FaTrash /> Delete
                         </button>
                     </span>
+
+                    <section>
+                        <h1>
+                            {' '}
+                            <FaFolderOpen /> Albuns liked:{' '}
+                        </h1>
+                        {UserAlbunsFavorites.length >= 1 ? (
+                            <h2>Ok</h2>
+                        ) : (
+                            <h2 className="ErrorBox">
+                                You don't have any album favorited{' '}
+                            </h2>
+                        )}
+                    </section>
+
+                    <section>
+                        <h1>
+                            <FaMusic /> Musics liked:{' '}
+                        </h1>
+                        {UserMusicsFavorites.length >= 1 ? (
+                            <h2>OK</h2>
+                        ) : (
+                            <h2 className="ErrorBox">
+                                You don't have any musics favorited{' '}
+                            </h2>
+                        )}
+                    </section>
+
+                    <section>
+                        <h1>
+                            <FaBroom /> Artists liked:{' '}
+                        </h1>
+                        {UserArtistsFavorites.length >= 1 ? (
+                            <h2>OK</h2>
+                        ) : (
+                            <h2 className="ErrorBox">
+                                You don't have any artist favorited{' '}
+                            </h2>
+                        )}
+                    </section>
                 </Container>
                 <Footer />
             </>
