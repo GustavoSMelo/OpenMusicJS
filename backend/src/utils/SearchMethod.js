468907cf6c9a1dde69c4bae3c2c6ec5b9af1537c
Gustavo Santos Melo <gsantos15569@gmail.com>
2020-03-09 23:33:48 -0300

    changing a searchmethod to index and show

--- backend/src/utils/SearchMethod.js
+++ backend/src/utils/SearchMethod.js
@@ -3,72 +3,83 @@ const music = require('../app/models/music');
 const artist = require('../app/models/artist');
 const user = require('../app/models/user');
 const album = require('../app/models/albuns');
+const authMethod = require('./authMethod');
 
-module.exports = async (req, res) => {
-    const { searchString } = req.body;
+module.exports = {
+    async index(req, res) {
+        const { searchString } = req.body;
 
-    if (!searchString) {
-        return res.status(400).json({
-            Error: 'Please, complete all the fields to continue ur action ',
+        if (!searchString) {
+            return res.status(400).json({
+                Error: 'Please, complete all the fields to continue ur action ',
+            });
+        }
+
+        const op = Sequelize.Op;
+        const info = [];
+
+        const SearchMusic = await music.findAll({
+            where: {
+                name: {
+                    [op.like]: `%${searchString}%`,
+                },
+            },
         });
-    }
 
-    const op = Sequelize.Op;
-    const info = [];
+        if (SearchMusic.length >= 1) {
+            info.push({ Musics: SearchMusic });
+        }
 
-    const SearchMusic = await music.findAll({
-        where: {
-            name: {
-                [op.like]: `%${searchString}%`,
+        const SearchArtists = await artist.findAll({
+            where: {
+                name_artistic: {
+                    [op.like]: `%${searchString}%`,
+                },
             },
-        },
-    });
+        });
 
-    if (SearchMusic.length >= 1) {
-        info.push({ Musics: SearchMusic });
-    }
+        if (SearchArtists.length >= 1) {
+            info.push({ Artists: SearchArtists });
+        }
 
-    const SearchArtists = await artist.findAll({
-        where: {
-            name_artistic: {
-                [op.like]: `%${searchString}%`,
+        const SearchAlbum = await album.findAll({
+            where: {
+                name: {
+                    [op.like]: `%${searchString}%`,
+                },
             },
-        },
-    });
+        });
 
-    if (SearchArtists.length >= 1) {
-        info.push({ Artists: SearchArtists });
-    }
+        if (SearchAlbum.length >= 1) {
+            info.push({ Albums: SearchAlbum });
+        }
 
-    const SearchAlbum = await album.findAll({
-        where: {
-            name: {
-                [op.like]: `%${searchString}%`,
+        const SearchUsers = await user.findAll({
+            where: {
+                name: {
+                    [op.like]: `%${searchString}%`,
+                },
             },
-        },
-    });
+        });
 
-    if (SearchAlbum.length >= 1) {
-        info.push({ Albums: SearchAlbum });
-    }
+        if (SearchUsers.length >= 1) {
+            info.push({ Users: SearchUsers });
+        }
 
-    const SearchUsers = await user.findAll({
-        where: {
-            name: {
-                [op.like]: `%${searchString}%`,
-            },
-        },
-    });
+        if (!info || info.length <= 0) {
+            return res
+                .status(404)
+                .json({ Error: 'Any artist, music, album or user is finded ' });
+        }
 
-    if (SearchUsers.length >= 1) {
-        info.push({ Users: SearchUsers });
-    }
+        return res.json(info);
+    },
 
-    if (!info || info.length <= 0) {
-        return res
-            .status(404)
-            .json({ Error: 'Any artist, music, album or user is finded ' });
-    }
+    async show(req, res) {
+        const AuthToken = req.headers.Authorization;
 
-    return res.json(info);
+        const info = authMethod(AuthToken);
+
+        return res.json(info);
+    },
 };
