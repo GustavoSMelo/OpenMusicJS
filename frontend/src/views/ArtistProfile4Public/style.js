1c681d33b2ab7a53eb4dff9df94145cec302d65b
Gustavo Santos Melo <gsantos15569@gmail.com>
2020-03-23 01:50:47 -0300

    style of a container music and error container

--- frontend/src/views/ArtistProfile4Public/style.js
+++ frontend/src/views/ArtistProfile4Public/style.js
@@ -2,7 +2,10 @@ import styled from 'styled-components';
 
 export const Container = styled.main`
     text-align: center;
-
+    margin: auto;
+    display: flex;
+    align-items: center;
+    flex-direction: column;
     .artistInfo {
         margin-top: 60px;
     }
@@ -46,4 +49,72 @@ export const Container = styled.main`
     figure > img {
         border-radius: 120px;
     }
+
+    .artistMusic > article {
+        margin: auto;
+        background-color: #fff;
+        width: 780px;
+        color: #000;
+        margin: 30px;
+        display: flex;
+        padding: 50px;
+        text-align: left;
+    }
+
+    .artistMusic > article > figure > img {
+        width: 175px;
+        height: 175px;
+        border-radius: 100px;
+        margin-right: 50px;
+    }
+
+    .artistMusic h1 {
+        font-size: 20pt;
+    }
+    .artistMusic h2 {
+        text-transform: capitalize;
+    }
+
+    .artistMusic button {
+        width: 300px;
+        padding: 10px;
+        margin-top: 30px;
+        border: none;
+        background-color: #b689e8;
+        cursor: pointer;
+        color: #fff;
+        border-radius: 5px;
+        border: solid 3px #fff;
+        transition: 0.3s;
+    }
+    .artistMusic button:hover {
+        transition: 0.3s;
+        background-color: #fff;
+        color: #b689e8;
+        border: solid 3px #b689e8;
+    }
+
+    @media screen and (max-width: 790px) {
+        .artistMusic > article {
+            width: 100%;
+            margin: 0px;
+            margin-bottom: 30px;
+            padding-top: 10px;
+            padding-bottom: 10px;
+        }
+
+        .artistMusic > article > figure > img {
+            margin-right: 10px;
+        }
+    }
+`;
+
+export const ContainerError = styled.article`
+    margin: auto;
+    background-color: #ff767b !important;
+    color: #ff1000 !important;
+    border-radius: 5px;
+    margin-top: 30px;
+    padding: 10px;
+    text-align: center;
 `;
