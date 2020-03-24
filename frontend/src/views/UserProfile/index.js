bb753b97bad0d905f73ff6d20dc5641c6606ae02
Gustavo Santos Melo <gsantos15569@gmail.com>
2020-03-24 18:31:03 -0300

    rename folder

---
+++ frontend/src/views/UserProfile/index.js
@@ -0,0 +1,226 @@
+import React, { useEffect, useState } from 'react';
+import authToken from '../../utils/authToken';
+import Navbar from '../../components/navbar';
+import Footer from '../../components/footer';
+import {
+    FaEnvelope,
+    FaPen,
+    FaTrash,
+    FaFolderOpen,
+    FaMusic,
+    FaBroom,
+    FaDoorOpen,
+    FaExclamation,
+    FaSmileBeam,
+} from 'react-icons/fa';
+import { Container, PopupContainer, ContainerError } from './styled';
+import DoLogin from '../../components/Layout/DoLogin';
+import { Link } from 'react-router-dom';
+import api from '../../api';
+
+function Profile() {
+    const [user, setUser] = useState('');
+    const [UserAlbunsFavorites, setUserAlbunsFavorites] = useState('');
+    const [UserMusicsFavorites, setUserMusicsFavorites] = useState('');
+    const [UserArtistsFavorites, setUserArtistsFavorites] = useState('');
+    const [popup, setPopup] = useState(false);
+    const [StatusPopup, setStatusPopup] = useState(<></>);
+    const [pass, setPass] = useState('');
+
+    useEffect(() => {
+        async function getApiData() {
+            try {
+                const datas = await authToken('/user/show');
+                console.log(datas);
+                await setUser(datas.data.user);
+                await setUserAlbunsFavorites(
+                    datas.data.user_albuns.dataUserAlbuns
+                );
+                await setUserArtistsFavorites(
+                    datas.data.user_artist.dataUserArtists
+                );
+                await setUserMusicsFavorites(
+                    datas.data.user_musics.dataUserMusics
+                );
+            } catch (err) {
+                console.error('Some error happens ');
+            }
+        }
+        getApiData();
+    }, []);
+
+    function HandlerUserExit() {
+        localStorage.setItem('token', '');
+        document.location = 'http://localhost:3000/';
+    }
+
+    async function handlerDeleteButton(e) {
+        e.preventDefault();
+
+        try {
+            await api.delete('/user', {
+                headers: {
+                    Authorization: localStorage.getItem('token'),
+                    pass,
+                },
+            });
+
+            document.location = 'http://localhost:3000';
+            localStorage.setItem('token', '');
+        } catch (err) {
+            console.log({ Error: err });
+            await setStatusPopup(
+                <ContainerError>
+                    <h1>Password invalid !</h1>
+                </ContainerError>
+            );
+        }
+    }
+
+    function LayoutDeleteUser() {
+        if (popup) {
+            return (
+                <PopupContainer>
+                    <h1>Do you want to leave us ? ;-;</h1>
+                    <small>
+                        That's okay... for it, you need to inform your password
+                    </small>
+                    {StatusPopup}
+                    <form>
+                        <input
+                            type="password"
+                            placeholder="Inform your password "
+                            required="required"
+                            onChange={e => setPass(e.target.value)}
+                            value={pass}
+                        />
+
+                        <br />
+                        <button
+                            type="submit"
+                            className="lastButtonDelete"
+                            onClick={handlerDeleteButton}
+                        >
+                            <FaExclamation />
+                            Delete my account
+                        </button>
+
+                        <button
+                            type="button"
+                            className="Nevermind"
+                            onClick={ChangeAttributePopup}
+                        >
+                            <FaSmileBeam /> Nevermind
+                        </button>
+                    </form>
+                </PopupContainer>
+            );
+        }
+    }
+
+    function ChangeAttributePopup() {
+        if (popup === true) {
+            setPopup(false);
+        } else {
+            setPopup(true);
+        }
+    }
+
+    function RenderWithUser() {
+        return (
+            <>
+                <Navbar />
+                {LayoutDeleteUser()}
+                <Container>
+                    <img
+                        src={`http://localhost:3333/img/${user.avatar}`}
+                        alt="avatar_profile"
+                    />
+                    <h1>{user.name}</h1>
+                    <h2>
+                        {' '}
+                        <FaEnvelope /> {user.email}
+                    </h2>
+
+                    <span>
+                        <Link
+                            to={{
+                                pathname: '/edit/profile',
+                                state: {
+                                    oldNameUser: user.name,
+                                    oldEmailUser: user.email,
+                                    oldAvatarUser: user.avatar,
+                                    idUser: user.id,
+                                },
+                            }}
+                            className="btnEdit"
+                        >
+                            <FaPen /> Edit
+                        </Link>
+                        <button
+                            className="btnDelete"
+                            onClick={ChangeAttributePopup}
+                        >
+                            <FaTrash /> Delete
+                        </button>
+                        <button
+                            className="btnExit"
+                            onClick={() => HandlerUserExit()}
+                        >
+                            <FaDoorOpen /> Exit
+                        </button>
+                    </span>
+
+                    <section>
+                        <h1>
+                            {' '}
+                            <FaFolderOpen /> Albuns liked:{' '}
+                        </h1>
+                        {UserAlbunsFavorites.length >= 1 ? (
+                            <h2>Ok</h2>
+                        ) : (
+                            <h2 className="ErrorBox">
+                                You don't have any album favorited{' '}
+                            </h2>
+                        )}
+                    </section>
+
+                    <section>
+                        <h1>
+                            <FaMusic /> Musics liked:{' '}
+                        </h1>
+                        {UserMusicsFavorites.length >= 1 ? (
+                            <h2>OK</h2>
+                        ) : (
+                            <h2 className="ErrorBox">
+                                You don't have any musics favorited{' '}
+                            </h2>
+                        )}
+                    </section>
+
+                    <section>
+                        <h1>
+                            <FaBroom /> Artists liked:{' '}
+                        </h1>
+                        {UserArtistsFavorites.length >= 1 ? (
+                            <h2>OK</h2>
+                        ) : (
+                            <h2 className="ErrorBox">
+                                You don't have any artist favorited{' '}
+                            </h2>
+                        )}
+                    </section>
+                </Container>
+                <Footer />
+            </>
+        );
+    }
+
+    function RenderWithOutUser() {
+        return <DoLogin />;
+    }
+
+    return <>{user ? RenderWithUser() : RenderWithOutUser()}</>;
+}
+
+export default Profile;
---
+++ frontend/src/views/UserProfile/styled.js
@@ -0,0 +1,192 @@
+import styled from 'styled-components';
+
+export const Container = styled.main`
+    background-color: #303030;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    color: #fff;
+
+    img {
+        margin-top: 50px;
+        width: 380px;
+        height: 380px;
+        border-radius: 360px;
+        box-shadow: 1px 1px 3px #252525;
+    }
+
+    h1 {
+        margin-top: 50px;
+        font-weight: bold;
+        text-transform: capitalize;
+        font-size: 26pt;
+    }
+
+    h2 {
+        margin-top: 30px;
+        font-size: 18pt;
+    }
+
+    span {
+        margin-top: 30px;
+    }
+
+    section {
+        margin-bottom: 30px;
+    }
+
+    .btnEdit {
+        border-radius: 30px;
+        border: solid 3px #ffd53d;
+        background-color: #303030;
+        color: #ffd53d;
+        width: 120px;
+        margin: 10px;
+        cursor: pointer;
+        padding: 15px;
+        font-weight: bold;
+        transition: 0.3s;
+        text-decoration: none;
+    }
+
+    .btnDelete {
+        border-radius: 30px;
+        border: solid 3px #ff6d6d;
+        background-color: #303030;
+        color: #ff6d6d;
+        width: 120px;
+        margin: 10px;
+        cursor: pointer;
+        padding: 15px;
+        font-weight: bold;
+        transition: 0.3s;
+    }
+
+    .btnExit {
+        border-radius: 30px;
+        border: solid 3px #fff;
+        background-color: #303030;
+        color: #fff;
+        width: 120px;
+        margin: 10px;
+        cursor: pointer;
+        padding: 15px;
+        font-weight: bold;
+        transition: 0.3s;
+    }
+    .btnExit:hover {
+        background-color: #fff;
+        border: solid 3px #303030;
+        color: #303030;
+        transition: 0.3s;
+    }
+
+    .btnEdit:hover {
+        transition: 0.3s;
+        background-color: #ffd53d;
+        color: #303030;
+        box-shadow: 1px 1px 1px #ffd53d;
+    }
+
+    .btnDelete:hover {
+        transition: 0.3s;
+        background-color: #ff6d6d;
+        color: #303030;
+        box-shadow: 1px 1px 1px #ff6d6d;
+    }
+
+    .ErrorBox {
+        width: 100%;
+        background-color: #ff6d6d;
+        color: #ff3624;
+        text-align: center;
+        padding: 20px;
+        border-radius: 5px;
+    }
+
+    @media screen and (max-width: 440px) {
+        img {
+            width: 100%;
+            border-radius: 0px;
+        }
+    }
+`;
+
+export const PopupContainer = styled.section`
+    overflow: hidden;
+    margin: auto;
+    position: fixed;
+    z-index: 3;
+    width: 60%;
+    height: 60vh;
+    top: 0;
+    left: 0;
+    right: 0;
+    bottom: 0;
+    background-color: #fff;
+    border-radius: 10px;
+    text-align: center;
+    padding: 50px;
+
+    input {
+        width: 75%;
+        margin-top: 100px;
+        padding: 15px;
+        border-radius: 5px;
+        border: none;
+        box-shadow: 1px 1px 3px #303030;
+        font-size: 18pt;
+        font-weight: bold;
+    }
+
+    .lastButtonDelete {
+        background-color: #fff;
+        border: solid 3px #ff3624;
+        color: #ff3624;
+        border-radius: 20px;
+        padding: 20px;
+        cursor: pointer;
+        font-weight: bold;
+        font-size: 14pt;
+        margin: 10px;
+        transition: 0.3s;
+    }
+
+    .lastButtonDelete:hover {
+        background-color: #ff3624;
+        border: solid 3px #fff;
+        color: #fff;
+        transition: 0.3s;
+    }
+
+    .Nevermind {
+        background-color: #fff;
+        border: solid 3px #a3ff99;
+        color: #a3ff99;
+        border-radius: 20px;
+        padding: 20px;
+        cursor: pointer;
+        font-weight: bold;
+        font-size: 16pt;
+        margin: 10px;
+        transition: 0.3s;
+    }
+
+    .Nevermind:hover {
+        background-color: #a3ff99;
+        border: solid 3px #fff;
+        color: #fff;
+        transition: 0.3s;
+    }
+`;
+
+export const ContainerError = styled.div`
+    background-color: #ff767b;
+    color: #ff1000;
+    padding: 50px;
+    text-align: center;
+    width: 75%;
+    margin-top: 30px;
+    border-radius: 10px;
+    margin: auto;
+`;
