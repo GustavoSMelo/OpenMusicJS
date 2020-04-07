c4807c1960fe996bc2eb30033ba6b8dd93180b2a
Gustavo Santos Melo <gsantos15569@gmail.com>
2020-04-06 23:58:02 -0300

    add logic of likes in this view

--- frontend/src/views/ArtistProfile4Public/index.js
+++ frontend/src/views/ArtistProfile4Public/index.js
@@ -4,34 +4,78 @@ import Navbar from '../../components/navbar';
 import DoLogin from '../../components/Layout/DoLogin';
 import Global from './global';
 import { Container, ContainerError } from './style';
-import { FaHeart, FaHeadphones } from 'react-icons/fa';
+import { FaHeadphones, FaHeart, FaRegHeart } from 'react-icons/fa';
 import MP3Player from '../../components/player';
 
 function ArtistProfile4Public(props) {
     const [haveInfo, setHaveInfo] = useState([]);
     const [PathMusic, setPathMusic] = useState('');
-    useEffect(() => {
-        async function getAPIInfo() {
-            try {
-                const { idArtist } = props;
-                const info = await api.post(
-                    '/artist/info',
-                    { idArtist },
-                    {
-                        headers: {
-                            Authorization: localStorage.getItem('token'),
-                        },
-                    }
-                );
-                console.log(info.data);
-                setHaveInfo(info.data);
-            } catch (err) {
-                console.error({ error: err });
-            }
-        }
+    const [likeArtist, setLikeArtist] = useState([]);
+    const [giveLike, setGiveLike] = useState(false);
 
+    async function getAPIInfo() {
+        try {
+            const { idArtist } = props;
+            const info = await api.post(
+                '/artist/info',
+                { idArtist },
+                {
+                    headers: {
+                        Authorization: localStorage.getItem('token'),
+                    },
+                }
+            );
+
+            const likeInfo = await api.get('/users/artists', {
+                headers: {
+                    Authorization: localStorage.getItem('token'),
+                },
+            });
+
+            await setHaveInfo(info.data);
+            await setLikeArtist(likeInfo.data);
+            await setGiveLike(false);
+        } catch (err) {
+            console.error({ error: err });
+        }
+    }
+
+    useEffect(() => {
         getAPIInfo();
-    }, []);
+    }, [giveLike]);
+
+    async function addLikeArtist(artist) {
+        try {
+            await api.post(
+                '/users/artists',
+                { artist },
+                {
+                    headers: {
+                        Authorization: localStorage.getItem('token'),
+                    },
+                }
+            );
+
+            await setGiveLike(true);
+        } catch (err) {
+            console.error({ err });
+        }
+    }
+
+    async function removeLikeArtist(artist) {
+        try {
+            await api.delete('/users/artists', {
+                headers: {
+                    Authorization: localStorage.getItem('token'),
+                    artist,
+                },
+            });
+
+            await setGiveLike(true);
+        } catch (err) {
+            console.error({ err });
+        }
+    }
 
     async function playMusic(music) {
         await setPathMusic(null);
@@ -56,19 +100,37 @@ function ArtistProfile4Public(props) {
                             <h1>
                                 {haveInfo.Artist.name_artistic} |{' '}
                                 {haveInfo.Artist.name}{' '}
-                            </h1>
-                            <button>
-                                {' '}
-                                <span className="icon-hearth">
-                                    <FaHeart />
-                                </span>{' '}
-                                Like
-                            </button>
+                            </h1>{' '}
+                            {likeArtist.find(
+                                (item) => haveInfo.Artist.id === item.artist
+                            ) ? (
+                                <button
+                                    onClick={() =>
+                                        removeLikeArtist(haveInfo.Artist.id)
+                                    }
+                                    className="liked"
+                                >
+                                    <span>
+                                        <FaHeart /> Dislike
+                                    </span>
+                                </button>
+                            ) : (
+                                <button
+                                    onClick={() =>
+                                        addLikeArtist(haveInfo.Artist.id)
+                                    }
+                                    className="notLiked"
+                                >
+                                    <span>
+                                        <FaRegHeart /> Like
+                                    </span>
+                                </button>
+                            )}{' '}
                         </section>
                         <section className="artistMusic">
                             <h1>Musics</h1>
                             {haveInfo.Musics.musicsOfArtists.length >= 1 ? (
-                                haveInfo.Musics.musicsOfArtists.map(item => (
+                                haveInfo.Musics.musicsOfArtists.map((item) => (
                                     <article key={item.id}>
                                         <figure>
                                             <img
