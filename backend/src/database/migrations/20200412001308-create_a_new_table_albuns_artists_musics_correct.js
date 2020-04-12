47e105e29c9dd00062617b1075c49a7bb4e6db13
Gustavo Santos Melo <gsantos15569@gmail.com>
2020-04-11 23:43:04 -0300

    new migration

---
+++ backend/src/database/migrations/20200412001308-create_a_new_table_albuns_artists_musics_correct.js
@@ -0,0 +1,55 @@
+'use strict';
+
+module.exports = {
+    up: (queryInterface, Sequelize) => {
+        return queryInterface.createTable('artist_albuns_musics', {
+            id: {
+                type: Sequelize.INTEGER,
+                allowNull: false,
+                primaryKey: true,
+                autoIncrement: true,
+            },
+
+            album: {
+                type: Sequelize.INTEGER,
+                allowNull: false,
+                references: {
+                    key: 'id',
+                    model: 'albuns',
+                },
+            },
+
+            music: {
+                type: Sequelize.INTEGER,
+                allowNull: false,
+                references: {
+                    key: 'id',
+                    model: 'musics',
+                },
+            },
+
+            artists: {
+                type: Sequelize.INTEGER,
+                allowNull: false,
+                references: {
+                    key: 'id',
+                    model: 'artists',
+                },
+            },
+
+            created_at: {
+                type: Sequelize.DATE,
+                allowNull: false,
+            },
+
+            updated_at: {
+                type: Sequelize.INTEGER,
+                allowNull: false,
+            },
+        });
+    },
+
+    down: (queryInterface, Sequelize) => {
+        return queryInterface.dropTable('artist_albuns_musics');
+    },
+};
