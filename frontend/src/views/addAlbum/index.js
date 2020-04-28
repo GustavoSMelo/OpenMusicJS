9e5aafe0962f6ebaa2546dce6be9eae50390a7c3
Gustavo Santos Melo <gsantos15569@gmail.com>
2020-04-28 18:35:35 -0300

    view to add a new album in application

---
+++ frontend/src/views/addAlbum/index.js
@@ -0,0 +1,119 @@
+import React, { useState } from 'react';
+import api from '../../api';
+import { Container, ContainerError, ContainerSucess } from './style';
+import Background from '../../assets/img/walp2.jpg';
+import {
+    FaMarker,
+    FaCompactDisc,
+    FaCalendarAlt,
+    FaPenAlt,
+    FaImage,
+    FaCheckCircle,
+} from 'react-icons/fa';
+import DoLogin from '../../components/Layout/DoLogin';
+
+function AddAlbum(props) {
+    const [name, setName] = useState('');
+    const [genre, setGenre] = useState('');
+    const [year_lunched, setYear_lunched] = useState(0);
+    const [description, setDescription] = useState('');
+    const [banner, setBanner] = useState([]);
+    const [status4Artist, setStatus4Artist] = useState(<></>);
+
+    async function handlerButtonClick() {
+        const fd = new FormData();
+        fd.append('name', name);
+        fd.append('genre', genre);
+        fd.append('year_lunched', year_lunched);
+        fd.append('description', description);
+        fd.append('banner', banner);
+
+        try {
+            await api.post('/album', fd, {
+                headers: {
+                    Authorization: localStorage.getItem('ArtistToken'),
+                    'content-type': `multipart/form-data; boundary=${fd.boundary}`,
+                },
+            });
+
+            await setStatus4Artist(
+                <ContainerSucess>Album created with success</ContainerSucess>
+            );
+        } catch (err) {
+            await setStatus4Artist(
+                <ContainerError>
+                    Error in create a album, please, try again
+                </ContainerError>
+            );
+        }
+    }
+
+    function Layout() {
+        if (props.artistID === null || props.artistID === undefined) {
+            return <DoLogin />;
+        }
+
+        return (
+            <Container img={Background}>
+                {status4Artist}
+                <section>
+                    <span>
+                        <FaMarker className="icon" />
+                        <input
+                            type="text"
+                            onChange={(e) => setName(e.target.value)}
+                            placeholder="Insert a name of album here "
+                            value={name}
+                            required="required"
+                        />
+                    </span>
+                    <span>
+                        <FaCompactDisc className="icon" />
+                        <input
+                            type="text"
+                            onChange={(e) => setGenre(e.target.value)}
+                            placeholder="Insert a genre of album here "
+                            value={genre}
+                            required="required"
+                        />
+                    </span>
+                    <span>
+                        <FaCalendarAlt className="icon" />
+                        <input
+                            type="number"
+                            onChange={(e) => setYear_lunched(e.target.value)}
+                            placeholder="Insert a year of release here "
+                            value={year_lunched}
+                            required="required"
+                        />
+                    </span>
+                    <span>
+                        <FaPenAlt className="icon" />
+                        <input
+                            type="text"
+                            onChange={(e) => setDescription(e.target.value)}
+                            placeholder="Insert a description here"
+                            value={description}
+                        />
+                    </span>
+                    <span>
+                        <FaImage className="icon" />
+                        <input
+                            type="file"
+                            onChange={(e) => setBanner(e.target.files[0])}
+                            required="required"
+                        />
+                    </span>
+
+                    <button type="button" onClick={handlerButtonClick}>
+                        <FaCheckCircle /> Add button
+                    </button>
+                </section>
+            </Container>
+        );
+    }
+
+    return Layout();
+}
+
+export default AddAlbum;
---
+++ frontend/src/views/addAlbum/style.js
@@ -0,0 +1,113 @@
+import styled from 'styled-components';
+
+export const Container = styled.main`
+    background-image: url(${(props) => props.img});
+    background-size: cover;
+    background-attachment: fixed;
+    background-repeat: no-repeat;
+    height: 100vh;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+
+    section {
+        padding: 30px;
+        background-color: #303030d0;
+        width: 50%;
+        display: flex;
+        flex-direction: column;
+        border-radius: 5px;
+        box-shadow: 1px 1px 6px #ffc7e4;
+        height: 60%;
+        align-items: center;
+    }
+
+    section > span {
+        display: flex;
+        width: 95%;
+        align-items: center;
+    }
+
+    section > span > .icon {
+        font-size: 24pt;
+        font-weight: bold;
+        color: #ffc7e4;
+    }
+
+    section > span > input {
+        padding: 15px;
+        margin: 15px;
+        border-radius: 5px;
+        background-color: #00000000;
+        border: none;
+        border-bottom: solid 5px #ffc7e4;
+        color: #ffc7e4;
+        font-size: 20pt;
+        font-weight: bold;
+        transition: 0.4s;
+        width: 75%;
+    }
+
+    section > span > input:focus {
+        width: 85%;
+        transition: 0.4s;
+        border-bottom: solid 10px #ffc7e4;
+        border-left: solid 5px #ffc7e4;
+    }
+
+    section > button {
+        background-color: #ffc7e4;
+        border-radius: 5px;
+        width: 265px;
+        padding: 20px;
+        font-size: 16pt;
+        font-weight: bold;
+        border: none;
+        cursor: pointer;
+    }
+
+    @media screen and (max-width: 920px) {
+        section {
+            width: 75%;
+            margin-top: 15%;
+        }
+    }
+
+    @media screen and (max-width: 720px) {
+        section {
+            width: 80%;
+            margin-top: 15%;
+        }
+    }
+`;
+
+export const ContainerError = styled.article`
+    background-color: #ffcfcf;
+    color: #f00;
+    font-size: 16pt;
+    text-align: center;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    margin: auto;
+    font-weight: bold;
+    border-radius: 5px;
+    padding: 25px;
+    margin: 10px;
+`;
+
+export const ContainerSucess = styled.article`
+    background-color: #cfe8d2;
+    color: #0f0;
+    font-size: 16pt;
+    text-align: center;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    margin: auto;
+    font-weight: bold;
+    border-radius: 5px;
+    padding: 25px;
+    margin: 10px;
+`;
