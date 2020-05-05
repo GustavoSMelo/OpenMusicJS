36474542845c07667f0c6834c9d1090de82f542c
Gustavo Santos Melo <gsantos15569@gmail.com>
2020-05-05 03:05:15 -0300

    connection with mongodb deleteted because i wont used mongodb in this projec

--- backend/src/config/mongodb/connection.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-mongoose.set('useCreateIndex', true);
-mongoose.connect('mongodb://localhost/spotify', {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-});
-
-module.exports = mongoose;
