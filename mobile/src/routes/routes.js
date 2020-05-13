5de5bb18da9de35cccbd42597501ed4522a741b3
Gustavo Santos Melo <gsantos15569@gmail.com>
2020-05-13 19:18:18 -0300

    new routes and style navigation in app

--- mobile/src/routes/routes.js
+++ mobile/src/routes/routes.js
@@ -1,15 +1,29 @@
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
-import Welcome from '../views/WelcomeView';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import Welcome from '../screens/WelcomeView';
+import Register from '../screens/Register';
+import Home from '../screens/Home';
 
 const Stack = createStackNavigator();
+const Tab = createBottomTabNavigator();
+
+function TabRoute() {
+    return (
+        <Tab.Navigator>
+            <Tab.Screen name="Home" component={Home} />
+        </Tab.Navigator>
+    );
+}
 
 function Routes() {
     return (
         <NavigationContainer>
             <Stack.Navigator screenOptions={{ headerShown: false }}>
                 <Stack.Screen name="Welcome" component={Welcome} />
+                <Stack.Screen name="Register" component={Register} />
+                <Stack.Screen name="Home" component={TabRoute} />
             </Stack.Navigator>
         </NavigationContainer>
     );
