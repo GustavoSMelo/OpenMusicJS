fd03d7a2b9fb327d8c04b0603afe37e322309c39
Gustavo Santos Melo <gsantos15569@gmail.com>
2020-05-18 01:32:07 -0300

    testing style home

---
+++ mobile/src/screens/Home/style.js
@@ -0,0 +1,8 @@
+import styled from 'styled-components/native';
+import darkmode from '../../styles/themes/dark';
+import LightMode from '../../styles/themes/light';
+
+export const Container = styled.View`
+    background-color: ${darkmode.backgroundcolor};
+    flex: 1;
+`;
