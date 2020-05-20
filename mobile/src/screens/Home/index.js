1dc0fff752b81ea7e149ecf55409705efc53dde4
Gustavo Santos Melo <gsantos15569@gmail.com>
2020-05-20 01:06:15 -0300

    logic of themes and try get token (in process)

--- mobile/src/screens/Home/index.js
+++ mobile/src/screens/Home/index.js
@@ -1,8 +1,62 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { AsyncStorage } from 'react-native';
+import api from '../../api/api';
 import { Container } from './style';
+import getTheme from '../../utils/getTheme';
+import DarkMode from '../../styles/themes/dark';
+import LightMode from '../../styles/themes/light';
+import Header from '../../components/header';
 
-function Home() {
-    return <Container></Container>;
+function Home(props) {
+    const [theme, setTheme] = useState('');
+    const [allMusics, setAllMusics] = useState([]);
+    const [likes, setLikes] = useState([]);
+
+    async function Theme() {
+        const theme = await getTheme();
+        setTheme(theme);
+    }
+
+    async function getDataByAPI() {
+        try {
+            console.log(props);
+            const token = await AsyncStorage.getItem('token');
+            const resolve = await api.get('/musics', {
+                headers: {
+                    Authorization: token,
+                },
+            });
+
+            await setAllMusics(resolve.data.allmusics);
+            await setLikes(resolve.data.likes_of_user);
+            return;
+        } catch (err) {
+            console.error(err);
+            return;
+        }
+    }
+
+    useEffect(() => {
+        Theme();
+        getDataByAPI();
+    }, []);
+
+    function Layout() {
+        if (theme === 'DarkMode') {
+            return (
+                <Container theme={DarkMode}>
+                    <Header theme={theme} />
+                </Container>
+            );
+        }
+        return (
+            <Container theme={LightMode}>
+                <Header theme={theme} />
+            </Container>
+        );
+    }
+
+    return Layout();
 }
 
 export default Home;
