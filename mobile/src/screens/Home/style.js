76debd5cf3109808576b0e628a6e0b0e154522c1
Gustavo Santos Melo <gsantos15569@gmail.com>
2020-05-20 01:06:33 -0300

    style of page home

--- mobile/src/screens/Home/style.js
+++ mobile/src/screens/Home/style.js
@@ -1,8 +1,6 @@
 import styled from 'styled-components/native';
-import darkmode from '../../styles/themes/dark';
-import LightMode from '../../styles/themes/light';
 
 export const Container = styled.View`
-    background-color: ${darkmode.backgroundcolor};
+    background-color: ${(props) => props.theme.backgroundcolor};
     flex: 1;
 `;
