493c6c12bf3b175e22d690fe758ccc39ac416209
Gustavo Santos Melo <gsantos15569@gmail.com>
2020-05-22 23:40:21 -0300

    working in this page

--- mobile/src/screens/Home/index.js
+++ mobile/src/screens/Home/index.js
@@ -1,11 +1,19 @@
 import React, { useState, useEffect } from 'react';
-import { AsyncStorage } from 'react-native';
+import { AsyncStorage, TouchableOpacity, FlatList } from 'react-native';
 import api from '../../api/api';
-import { Container } from './style';
+import {
+    Container,
+    MusicContainer,
+    Figure,
+    TextDark,
+    ButtonListen,
+    ActionContainer,
+} from './style';
 import getTheme from '../../utils/getTheme';
 import DarkMode from '../../styles/themes/dark';
 import LightMode from '../../styles/themes/light';
 import Header from '../../components/header';
+import Icons from 'react-native-vector-icons/FontAwesome';
 
 function Home(props) {
     const [theme, setTheme] = useState('');
@@ -19,9 +27,7 @@ function Home(props) {
 
     async function getDataByAPI() {
         try {
-            console.log(props);
             const token = await AsyncStorage.getItem('token');
-            console.log(token);
             const resolve = await api.get('/musics', {
                 headers: {
                     Authorization: `Bearer ${token}`,
@@ -37,17 +43,115 @@ function Home(props) {
         }
     }
 
+    async function handlerLikeAdd(music) {
+        try {
+            const token = await AsyncStorage.getItem('token');
+            await api.post(
+                '/users/musics',
+                { music },
+                { headers: { Authorization: `Bearer ${token}` } }
+            );
+
+            const newLikes = await api.get('/users/musics', {
+                headers: { Authorization: `Bearer ${token}` },
+            });
+
+            setLikes(newLikes);
+        } catch (err) {
+            console.error(err);
+        }
+    }
+
+    async function handlerLikeRemove(music) {
+        try {
+            const token = await AsyncStorage.getItem('token');
+            await api.delete('/users/musics', {
+                headers: {
+                    Authorization: `Bearer ${token}`,
+                    music,
+                },
+            });
+
+            const newLikes = await api.get('/users/musics', {
+                headers: { Authorization: `Bearer ${token}` },
+            });
+
+            setLikes(newLikes);
+        } catch (err) {
+            console.error(err);
+        }
+    }
+
     useEffect(() => {
         Theme();
         getDataByAPI();
-    }, []);
+    }, [likes]);
 
     function Layout() {
         if (theme === 'DarkMode') {
             return (
-                <Container theme={DarkMode}>
+                <>
                     <Header theme={theme} />
-                </Container>
+                    <Container theme={DarkMode}>
+                        <FlatList
+                            data={allMusics}
+                            onEndReached={getDataByAPI}
+                            onEndReachedThreshold={0.2}
+                            keyExtractor={(allMusics) => allMusics.id}
+                            renderItem={({ item: music }) => (
+                                <MusicContainer>
+                                    <Figure
+                                        source={{
+                                            uri: `http://192.168.0.104:3333/img/${music.banner_path}`,
+                                        }}
+                                        resizeMode="stretch"
+                                    />
+                                    <TextDark>{music.name}</TextDark>
+                                    <TextDark>{music.genre}</TextDark>
+                                    <ActionContainer>
+                                        <ButtonListen>
+                                            <TextDark>
+                                                <Icons
+                                                    name="headphones"
+                                                    size={18}
+                                                />{' '}
+                                                Listen
+                                            </TextDark>
+                                        </ButtonListen>
+
+                                        {likes.find(
+                                            (msc) => msc.music === music.id
+                                        ) ? (
+                                            <TouchableOpacity
+                                                onPress={() =>
+                                                    handlerLikeRemove(music.id)
+                                                }
+                                            >
+                                                <Icons
+                                                    name="heart"
+                                                    size={28}
+                                                    color={'#f00'}
+                                                />
+                                            </TouchableOpacity>
+                                        ) : (
+                                            <TouchableOpacity
+                                                onPress={() =>
+                                                    handlerLikeAdd(music.id)
+                                                }
+                                            >
+                                                <Icons
+                                                    name="heart-o"
+                                                    size={28}
+                                                    color={'#f00'}
+                                                />
+                                            </TouchableOpacity>
+                                        )}
+                                    </ActionContainer>
+                                </MusicContainer>
+                            )}
+                        />
+                    </Container>
+                </>
             );
         }
         return (
