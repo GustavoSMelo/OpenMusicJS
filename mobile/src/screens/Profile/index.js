1736985729aaf8ff5f34d1be773d321e2f982a57
Gustavo Santos Melo <gsantos15569@gmail.com>
2020-06-18 23:56:56 -0300

    changing baseURL address

--- mobile/src/screens/Profile/index.js
+++ mobile/src/screens/Profile/index.js
@@ -1,7 +1,80 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
+import { AsyncStorage } from 'react-native';
+import api from '../../api/api';
+import { Container,
+    Figure,
+    CustomText,
+    ButtonControlls,
+    DeleteButton,
+    EditButton,
+    ExitButton } from './style';
+import DarkTheme from '../../styles/themes/dark';
+import LightTheme from '../../styles/themes/light';
+import Icons from 'react-native-vector-icons/FontAwesome';
+import { useNavigation } from '@react-navigation/native';
+import axios from 'axios';
 
 function Profile() {
-    return <></>;
+    const [userInfo, setUserInfo] = useState({});
+    const [Theme, setTheme] = useState('');
+    const navigation = useNavigation();
+
+    async function getDataByAPI(){
+        try{
+            const responseTheme = await AsyncStorage.getItem('Theme');
+            const token = await AsyncStorage.getItem('token');
+            const response = await api.get('/user/show', { headers:{
+                Authorization: `Bearer ${token}`
+            }});
+
+            await setUserInfo(response.data.user);
+            await setTheme(responseTheme);
+        }catch(err){
+            navigation.navigate('Welcome');
+        }
+    }
+
+    async function handlerButtonExit(){
+        await AsyncStorage.setItem('token', '');
+        await AsyncStorage.setItem('email', '');
+        navigation.navigate('Welcome');
+    }
+
+    useEffect(() => {
+        getDataByAPI();
+    }, []);
+
+    function Layout(){
+        if(Theme === 'DarkMode'){
+            return (
+            <Container theme={DarkTheme}>
+                <Figure
+                source={{uri: `http://192.168.0.104:3333/img/${userInfo.avatar}`}}
+                resizeMode='stretch'/>
+                <CustomText theme={DarkTheme}>
+                    <Icons name='user' size={26}/> {userInfo.name}
+                    {'\n\n'}
+                    <Icons name='envelope-o' size={26}/> {userInfo.email}
+                </CustomText>
+
+                <ButtonControlls>
+                    <ExitButton onPress={() => handlerButtonExit()}>
+                        <Icons name='sign-out' size={26} />
+                    </ExitButton>
+                    <EditButton>
+                        <Icons name='edit' size={26} />
+                    </EditButton>
+                    <DeleteButton>
+                        <Icons name='trash' size={26} />
+                    </DeleteButton>
+                </ButtonControlls>
+            </Container>);
+        }
+
+        return <></>
+    }
+
+    return Layout();
 }
 
 export default Profile;
