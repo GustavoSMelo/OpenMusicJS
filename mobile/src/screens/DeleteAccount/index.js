b8b2d2f0f6937460e6890bcabe9428b4f56b4145
Gustavo Santos Melo <gsantos15569@gmail.com>
2020-06-19 23:55:17 -0300

    algorithm to delete account done

--- mobile/src/screens/DeleteAccount/index.js
+++ mobile/src/screens/DeleteAccount/index.js
@@ -3,18 +3,47 @@ import { Container,
     DeleteButton,
     Header,
     Form,
-    Input } from './styled';
-import { AsyncStorage } from 'react-native';
+    PasswordInput,
+    CustomText } from './styled';
+import { AsyncStorage, ToastAndroid, TouchableOpacity } from 'react-native';
 import Icons from 'react-native-vector-icons/FontAwesome';
+import api from '../../api/api';
+import { useNavigation } from '@react-navigation/native';
+import DarkTheme from '../../styles/themes/dark';
+import LightTheme from '../../styles/themes/light';
 
 function DeleteAccount() {
 	const [Theme, setTheme] = useState('');
 	const [pass, setPass] = useState('');
+	const navigation = useNavigation();
 
     async function getTheme() {
-		const response = await AsyncStorage.getItem('theme');
+		const response = await AsyncStorage.getItem('Theme');
 		await setTheme(response);
-    }
+	}
+
+	async function handlerDeleteAccount() {
+		if (pass === null || pass === '' || pass === undefined) {
+			return ToastAndroid.show('Error, any password inserted ', 5);
+		}
+
+		try {
+			const token = await AsyncStorage.getItem('token');
+			await api.delete('/user', { headers: {
+				authorization: `Bearer ${token}`,
+				pass,
+			}});
+
+			await AsyncStorage.setItem('token', '');
+			await AsyncStorage.setItem('email', '');
+
+			ToastAndroid.show('User deleted with sucess ', 5);
+
+			return navigation.navigate('Welcome');
+		} catch (err) {
+			return ToastAndroid.show('Error, you send a password wrong ', 5);
+		}
+	}
 
     useEffect(() => {
 		getTheme();
@@ -23,23 +52,52 @@ function DeleteAccount() {
     function Layout() {
 		if(Theme === 'DarkMode') {
 			return (<>
-				<Header></Header>
-				<Container>
+				<Header theme={DarkTheme}>
+                    <TouchableOpacity onPress={() => navigation.goBack()}>
+                        <Icons name='arrow-left' size={26} color={'#fff'} />
+                    </TouchableOpacity>
+                </Header>
+				<Container theme={DarkTheme}>
+					<CustomText theme={DarkTheme}>Are you have sure that</CustomText>
+					<CustomText theme={DarkTheme}>you want delete your account ? </CustomText>
 					<Form>
-						<Input placeholder='Insert your password here... '
-						onChangeText={setPass}
-						value={pass}/>
-						<DeleteButton>
+						<PasswordInput placeholder='Insert your password here... '
+							onChangeText={setPass}
+							value={pass}
+							secureTextEntry={true}
+							theme={DarkTheme}
+						/>
+						<DeleteButton onPress={() => handlerDeleteAccount()}>
 							<Icons name='trash' size={26} />
 						</DeleteButton>
 					</Form>
 				</Container>
 			</>)
 		}
-		return <></>;
+        return (<>
+            <Header theme={LightTheme}>
+                <TouchableOpacity onPress={() => navigation.goBack()}>
+                    <Icons name='arrow-left' size={26} color={'#fff'} />
+                </TouchableOpacity>
+            </Header>
+			<Container theme={LightTheme}>
+                <CustomText theme={LightTheme}>Are you have sure that</CustomText>
+				<CustomText theme={LightTheme}>you want delete your account ? </CustomText>
+				<Form>
+					<PasswordInput placeholder='Insert your password here... '
+							onChangeText={setPass}
+							value={pass}
+							secureTextEntry={true}
+							theme={LightTheme}
+						/>
+					<DeleteButton onPress={() => handlerDeleteAccount()}>
+						<Icons name='trash' size={26} />
+					</DeleteButton>
+				</Form>
+            </Container>
+        </>);
     }
-
-    return Layout;
+    return Layout();
 }
 
 export default DeleteAccount;
