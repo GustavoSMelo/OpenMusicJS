019c0c226b7cccd28f0b5cacac6b9c7e340c2d58
Gustavo Santos Melo <gsantos15569@gmail.com>
2020-06-25 01:13:12 -0300

    change CRLF to LF (end of line)

--- mobile/.eslintrc.js
+++ mobile/.eslintrc.js
@@ -1,25 +1,21 @@
 module.exports = {
-  env: {
-    es6: true,
-  },
-  extends: [
-    'plugin:react/recommended',
-    'airbnb',
-  ],
-  globals: {
-    Atomics: 'readonly',
-    SharedArrayBuffer: 'readonly',
-  },
-  parserOptions: {
-    ecmaFeatures: {
-      jsx: true,
+    env: {
+        es6: true,
     },
-    ecmaVersion: 2018,
-    sourceType: 'module',
-  },
-  plugins: [
-    'react',
-  ],
-  rules: {
-  },
+    extends: ['plugin:react/recommended', 'airbnb'],
+    globals: {
+        Atomics: 'readonly',
+        SharedArrayBuffer: 'readonly',
+    },
+    parserOptions: {
+        ecmaFeatures: {
+            jsx: true,
+        },
+        ecmaVersion: 2018,
+        sourceType: 'module',
+    },
+    plugins: ['react'],
+    rules: {
+        indent: ['error', 4],
+    },
 };
