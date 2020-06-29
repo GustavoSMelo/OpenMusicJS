815f63946101b7be4c06de70989317f0b34d2f13
Gustavo Santos Melo <gsantos15569@gmail.com>
2020-06-28 23:59:22 -0300

    view of search (working yet)

--- mobile/src/screens/Search/index.js
+++ mobile/src/screens/Search/index.js
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import { AsyncStorage, ToastAndroid, Text } from 'react-native';
+import Icons from 'react-native-vector-icons/FontAwesome';
 import {
     Header,
     Container,
@@ -10,12 +11,14 @@ import {
     InputSearch,
     Figure,
     TextInfo,
+    CustomButton,
+    SectionLabel,
 } from './style';
 import getTheme from '../../utils/getTheme';
 import DarkTheme from '../../styles/themes/dark';
 import LightTheme from '../../styles/themes/light';
-import Icons from 'react-native-vector-icons/FontAwesome';
 import api from '../../api/api';
+import { useNavigation } from '@react-navigation/native';
 
 function Search() {
     const [theme, setTheme] = useState('');
@@ -23,6 +26,7 @@ function Search() {
     const [musics, setMusics] = useState([]);
     const [artists, setArtists] = useState([]);
     const [albuns, setAlbuns] = useState([]);
+    const navigation = useNavigation();
 
     async function getThemeStorage() {
         const response = await getTheme();
@@ -52,13 +56,11 @@ function Search() {
                 }
             );
 
-            await setArtists([]);
-            await setAlbuns([]);
-            await setMusics([]);
+            await setMusics(response.data[0].Musics);
+            await setArtists(response.data[1].Artists);
+            await setAlbuns(response.data[2].Albums);
 
-            await setMusics([...musics, response.data.Musics]);
-            await setAlbuns([...albuns, response.data.Albums]);
-            await setArtists([...artists, response.data.Artists]);
+            console.log(musics);
         } catch (err) {
             return ToastAndroid.show('Error', 5);
         }
@@ -87,24 +89,94 @@ function Search() {
                         {musics.length <= 0 ? (
                             <></>
                         ) : (
-                            musics.map((music) => (
-                                <Cards key={music.id}>
-                                    <Figure>
-                                        <CardImage
-                                            source={{
-                                                uri:
-                                                    'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ffthmb.tqn.com%2FCalyBE0j7MuRgOZpXEVKJDgjQoY%3D%2F1093x725%2Ffilters%3Afill(auto%2C1)%2Fitunes-on-linux-5738ca5b5f9b58723d8b1137.jpg&f=1&nofb=1',
-                                            }}
-                                            resizeMode='contain'
-                                        />
-                                    </Figure>
-                                    <CardInfo>
-                                        <TextInfo theme={DarkTheme}>
-                                            Testando o visual
-                                        </TextInfo>
-                                    </CardInfo>
-                                </Cards>
-                            ))
+                            <>
+                                <SectionLabel theme={DarkTheme}>
+                                    Musics:{' '}
+                                </SectionLabel>
+                                {musics.map((msc) => (
+                                    <Cards key={msc.id}>
+                                        <Figure>
+                                            <CardImage
+                                                source={{
+                                                    uri: `http://192.168.0.102:3333/img/${msc.banner_path}`,
+                                                }}
+                                            />
+                                        </Figure>
+                                        <CardInfo>
+                                            <TextInfo theme={DarkTheme}>
+                                                {`${msc.name}`}
+                                                {`\n${msc.genre}`}
+                                            </TextInfo>
+                                            <CustomButton
+                                                onPress={() =>
+                                                    navigation.navigate(
+                                                        'Sound',
+                                                        {
+                                                            image:
+                                                                msc.banner_path,
+                                                            name: msc.name,
+                                                            sound: msc.path,
+                                                        }
+                                                    )
+                                                }
+                                            >
+                                                <TextInfo theme={DarkTheme}>
+                                                    <Icons
+                                                        name='headphones'
+                                                        size={26}
+                                                        color='#fff'
+                                                    />{' '}
+                                                    Listen
+                                                </TextInfo>
+                                            </CustomButton>
+                                        </CardInfo>
+                                    </Cards>
+                                ))}
+                            </>
+                        )}
+
+                        {artists.length <= 0 ? (
+                            <></>
+                        ) : (
+                            <>
+                                <SectionLabel theme={DarkTheme}>
+                                    Artistis:{' '}
+                                </SectionLabel>
+                                {artists.map((arts) => (
+                                    <Cards key={arts.id}>
+                                        <Figure>
+                                            <CardImage
+                                                source={{
+                                                    uri: `http://192.168.0.102:3333/img/${arts.avatar}`,
+                                                }}
+                                            />
+                                        </Figure>
+                                        <CardInfo>
+                                            <TextInfo
+                                                theme={DarkTheme}
+                                            >{`${arts.name} | ${arts.name_artistic}`}</TextInfo>
+                                            <CustomButton
+                                                onPress={() =>
+                                                    navigation.navigate(
+                                                        'ArtistProfile',
+                                                        {
+                                                            avatar: `http://192.168.0.102:3333/img/${arts.avatar}`,
+                                                            art_id: arts.id,
+                                                            name: arts.name,
+                                                            artistic_name:
+                                                                arts.name_artistic,
+                                                        }
+                                                    )
+                                                }
+                                            >
+                                                <TextInfo theme={DarkTheme}>
+                                                    Profile
+                                                </TextInfo>
+                                            </CustomButton>
+                                        </CardInfo>
+                                    </Cards>
+                                ))}
+                            </>
                         )}
                     </Container>
                 </>
