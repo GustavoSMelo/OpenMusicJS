5539b49e90de5dd20afb688633fdacbff27077fe
Gustavo Santos Melo <gsantos15569@gmail.com>
2020-07-01 23:59:56 -0300

    remaking the page

--- frontend/src/views/InsertMusicInAlbum/style.js
+++ frontend/src/views/InsertMusicInAlbum/style.js
@@ -3,74 +3,61 @@ import styled from 'styled-components';
 export const Container = styled.main`
     display: flex;
     flex-direction: row;
+    padding-left: 30px;
+    padding-right: 30px;
+    padding-top: 10px;
     justify-content: space-between;
+    align-items: center;
 
-    figure {
-        background-color: #70707030;
-        padding: 15px;
-        margin: 30px;
-        border-radius: 30px;
-        box-shadow: 1px 1px 4px #909090;
+    aside,
+    section,
+    article {
+        width: 45%;
     }
 
-    img {
-        width: 300px;
-        height: 180px;
-    }
-
-    section > figure > button {
-        display: flex;
-        align-items: center;
-        justify-content: center;
-        background-color: #ff0608;
-        padding: 10px;
+    .ControlButtonInsert {
+        background-color: #00d932;
         border-radius: 120px;
-        font-size: 16pt;
-        border: none;
-        height: 60px;
-        width: 60px;
-        color: #fff;
-        position: absolute;
-        cursor: pointer;
-        transition: 0.5s;
-        font-weight: bold;
-    }
-
-    section > figure > button:hover {
-        background-color: #fff;
-        border: solid 4px #ff0608;
-        color: #ff0608;
-        transition: 0.25s;
-        box-shadow: 1px 1px 3px #606060;
-    }
-
-    section > figure {
         text-align: center;
-    }
-
-    article > figure > button {
-        display: flex;
-        align-items: center;
         justify-content: center;
-        background-color: #aae89b;
-        padding: 10px;
-        border-radius: 120px;
-        font-size: 16pt;
-        border: none;
-        height: 60px;
-        width: 60px;
         color: #fff;
-        position: absolute;
+        border: none;
+        padding: 15px;
+        font-size: 16pt;
+        width: 50px;
+        height: 50px;
         cursor: pointer;
-        transition: 0.5s;
-        font-weight: bold;
     }
 
-    article > figure > button:hover {
-        background-color: #fff;
-        border: solid 4px #aae89b;
-        color: #aae89b;
-        transition: 0.25s;
-        box-shadow: 1px 1px 3px #606060;
+    .ControlButtonDelete {
+        background-color: #f01003;
+        border-radius: 360px;
+        text-align: center;
+        justify-content: center;
+        align-items: center;
+        color: #fff;
+        border: none;
+        padding: 15px;
+        font-size: 16pt;
+        width: 50px;
+        height: 50px;
+        cursor: pointer;
     }
+`;
+
+export const Card = styled.article`
+    width: 100%;
+    border-radius: 5px;
+    box-shadow: 1px 1px 9px #707070;
+    height: 300px;
+    align-items: flex-start;
+
+    text-align: center;
+`;
+
+export const Pictures = styled.img`
+    width: 100%;
+    height: 150px;
+    border-top-left-radius: 5px;
+    border-top-right-radius: 5px;
 `;
